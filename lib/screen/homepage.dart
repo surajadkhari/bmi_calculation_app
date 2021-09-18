5899b3bb94a11e6ab7654f517aefa459aae7d2a8
surajadkhari <surajadkhari@gmail.com>
2021-09-18 20:05:42 +0545

    commit

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -14,6 +14,59 @@ class _BMICalState extends State<BMICal> {
       appBar: AppBar(
         title: Text("BMI calculatio"),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReuseableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+              child: ReuseableCard(
+            colour: Color(0xFF1D1E33),
+          )),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                  child: ReuseableCard(
+                colour: Color(0xFF1D1E33),
+              )),
+              Expanded(
+                  child: ReuseableCard(
+                colour: Color(0xFF1D1E33),
+              ))
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+  final Color colour;
+  ReuseableCard({required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 }
